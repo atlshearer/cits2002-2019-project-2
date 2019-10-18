e954547643a78bf38bf02eda477c74b79931e28b
happythe3 <alexshearer00@gmail.com>
2019-10-18 11:13:08 +0800

    Fixed removing linked file

--- library/rmfile.c
+++ library/rmfile.c
@@ -16,92 +16,121 @@ int SIFS_rmfile(const char *volumename, const char *pathname)
      #define    SIFS_ENOTFILE    8    // Not a file
      #define    SIFS_ENOMEM    11    // Memory allocation failed
      */
-    
+
     FILE *vol = fopen(volumename, "r+");
     SIFS_VOLUME_HEADER header;
-    char** parsed_path = NULL;
-    size_t path_depth  = 0;
-    
-    SIFS_DIRBLOCK  parent;
+    char **parsed_path = NULL;
+    size_t path_depth = 0;
+
+    SIFS_DIRBLOCK dir_buffer;
+    SIFS_DIRBLOCK parent;
     SIFS_FILEBLOCK target;
-    
+
     SIFS_BLOCKID parent_id;
     SIFS_BLOCKID target_id;
-    
-    SIFS_BIT out_buffer;
-    
-    
+
+    SIFS_BIT bit_buffer;
+
     if (vol == NULL)
     {
         SIFS_errno = SIFS_ENOVOL;
         return 1;
     }
-    
+
     SIFS_getheader(vol, &header);
-    
+
     parsed_path = SIFS_parsepathname(pathname, &path_depth);
-    
-    if (SIFS_getfileblockid(vol, parsed_path, path_depth, header, &parent_id, &target_id) != 0) {
+
+    if (SIFS_getfileblockid(vol, parsed_path, path_depth, header, &parent_id, &target_id) != 0)
+    {
         return 1;
     }
-    
-    if (SIFS_getdirblock(vol, parent_id, header, &parent) != 0) {
+
+    if (SIFS_getdirblock(vol, parent_id, header, &parent) != 0)
+    {
         return 1;
     }
-    
-    if (SIFS_getfileblock(vol, target_id, header, &target) != 0) {
+
+    if (SIFS_getfileblock(vol, target_id, header, &target) != 0)
+    {
         return 1;
     }
-    
+
     // find target index
-    for (size_t i = 0; i < parent.nentries; i++) {
+    for (size_t i = 0; i < parent.nentries; i++)
+    {
         if (parent.entries[i].blockID == target_id &&
-            strcmp(target.filenames[parent.entries[i].fileindex], parsed_path[path_depth - 1]) == 0) {
-            printf("target_id = %i\n", target_id);
+            strcmp(target.filenames[parent.entries[i].fileindex], parsed_path[path_depth - 1]) == 0)
+        {
             // Check if other copies exist
-            if (target.nfiles > 1) {
-                printf("target.nfiles = %i\n", target.nfiles);
+            if (target.nfiles > 1)
+            {
                 // Remove filename from target
-                for (size_t j = parent.entries[i].fileindex; j < target.nfiles - 1; j++) {
+                for (size_t j = parent.entries[i].fileindex; j < target.nfiles - 1; j++)
+                {
                     strcpy(target.filenames[j], target.filenames[j + 1]);
                 }
-                
+
                 target.nfiles--;
-                printf("target.nfiles = %i\n", target.nfiles);
+
+                // Update fileindex in all dir that 'contain' target
+                for (size_t j = 0; j < header.blocksize; j++)
+                {
+                    if (SIFS_getblocktype(vol, j, header, &bit_buffer) != 0) {
+                        return 1;
+                    }
+
+                    if (bit_buffer == SIFS_DIR)
+                    {
+                        SIFS_getdirblock(vol, j, header, &dir_buffer);
+
+                        for (size_t entry_idx = 0; entry_idx < dir_buffer.nentries; entry_idx++)
+                        {
+                            if (dir_buffer.entries[entry_idx].blockID == target_id && 
+                                dir_buffer.entries[entry_idx].fileindex > parent.entries[i].fileindex)
+                            {
+                                dir_buffer.entries[entry_idx].fileindex--;
+                            }
+                        }
+
+                        SIFS_writedirblock(vol, j, header, &dir_buffer);
+                    }
+                }
                 
                 // Write back to file
                 SIFS_writefileblock(vol, target_id, header, &target);
-            } else {
-                // Remove file from archive
-                out_buffer = SIFS_UNUSED;
-                SIFS_writeblocktype(vol, target_id, header, &out_buffer, 1);
-                printf("target.firstblockID = %i target.length = %zu\n", target.firstblockID, target.length);
-                SIFS_writeblocktype(vol, target.firstblockID, header, &out_buffer, (target.length + header.blocksize - 1) / header.blocksize);
             }
-            
+            else
+            {
+                // Remove file from archive
+                bit_buffer = SIFS_UNUSED;
+                SIFS_writeblocktype(vol, target_id, header, &bit_buffer, 1);
+                SIFS_writeblocktype(vol, target.firstblockID, header, &bit_buffer, (target.length + header.blocksize - 1) / header.blocksize);
+            }
+
+            // Is possible that fileindex will have been altered, need to refresh parent
+            SIFS_getdirblock(vol, parent_id, header, &parent);
+
             // Remove reference in parent
-            for (size_t j = i; j < parent.nentries - 1; j++) {
+            for (size_t j = i; j < parent.nentries - 1; j++)
+            {
                 parent.entries[j] = parent.entries[j + 1];
             }
-            
+
             parent.nentries--;
-            
+
             // Write back to file
-            SIFS_DIRBLOCK tmp;
-            SIFS_getdirblock(vol, parent_id, header, &tmp);
-            if (SIFS_writedirblock(vol, parent_id, header, &parent) != 0) {
+            if (SIFS_writedirblock(vol, parent_id, header, &parent) != 0)
+            {
                 return 1;
             }
-            SIFS_getdirblock(vol, parent_id, header, &tmp);
-            
+
             break;
         }
     }
 
-    
     fclose(vol);
-    
-    SIFS_errno	= SIFS_EOK;
+
+    SIFS_errno = SIFS_EOK;
     return 0;
 }
-
